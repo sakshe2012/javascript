71410e55c8a0d0713bba0268e3e310c953a28b2e
SakshiKumbhar <sakshikumbhar@orchidengg.ac.in>
2026-06-04 13:35:38 +0530

    Add files via upload

---
+++ sixth.js
@@ -0,0 +1,3 @@
+let h2= document.querySelector("h2");  // accesed the element which we have to chsnge text
+console.dir(h2.innerText);// we havae to appen the new text tto orginial text therefore we used innerText fucntion
+h2.innerText=h2.innerText + " from sakshi kumbhar";
